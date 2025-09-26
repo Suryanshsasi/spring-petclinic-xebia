e679fd15d0bc0c5072bcf3c4664dbfc820a189a3
SuryanshTK <42539233+Suryanshsasi@users.noreply.github.com>
2025-09-26 09:34:09 +0200

    Define new variables for Terraform configuration
    Added new variables for app configuration and deployment.
    
    Signed-off-by: SuryanshTK <42539233+Suryanshsasi@users.noreply.github.com>

--- infra/infra-eu-central-1/variables.tf
+++ infra/infra-eu-central-1/variables.tf
@@ -1,7 +1,30 @@
-# Variables for Terraform
-
-variable "region" {
-  description = "AWS region"
-  default     = "eu-central-1"
+variables.tf
+variable "app_name" {
+  type    = string
+  default = "petclinic"
 }
 
+variable "region" {
+  type    = string
+  default = "eu-central-1"
+}
+
+variable "container_port" {
+  type    = number
+  default = 8080
+}
+
+variable "desired_count" {
+  type    = number
+  default = 2
+}
+
+variable "ecr_repo_name" {
+  type    = string
+  default = "petclinic"
+}
+
+variable "image_tag" {
+  type    = string
+  default = "latest"
+}
